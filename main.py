5067bfc0577392089ae3eba4cd2ef324b7054e55
birdiebairdie <birdiebairdie@gmail.com>
2025-02-10 16:30:48 -0500

    added charcount function

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
-def main():
-  with open("books/frankenstein.txt") as f:
+def main(book):
+  file_contents = ""
+  with open(book) as f:
     file_contents = f.read()
-  print(file_contents)
+  return file_contents
 
-def wordcount():
+def wordcount(book):
   wordcount = 0
-  with open("books/frankenstein.txt") as f:
-    file_contents = f.read()
-    words = file_contents.split()
+  #with open("books/frankenstein.txt") as f:
+  #  file_contents = f.read()
+  words = main(book).split()
   for word in words:
     wordcount += 1
   print(wordcount)
 
-# main()
-wordcount()
+def charcount(book):
+  char_dict = {}
+  lowercase_book = main(book).lower()
+  char_list = list(lowercase_book)
+  for char in char_list:
+    if char in char_dict:
+      char_dict[char] += 1
+    else:
+      char_dict[char] = 1
+  return char_dict
+
+# main("books/frankenstein.txt")
+# wordcount()
+char_count = charcount("books/frankenstein.txt")
+print(char_count)
