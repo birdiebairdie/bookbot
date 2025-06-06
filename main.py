672152226fdebc3b2812481ac09027d2ee793831
birdiebairdie <birdiebairdie@gmail.com>
2025-06-06 10:32:55 -0400

    refactoring all code and replacing old version of frankenstein.txt

--- main.py
+++ main.py
@@ -1,51 +1,36 @@
-def main(book):
-  file_contents = ""
-  with open(book) as f:
-    file_contents = f.read()
-  return file_contents
-
-def wordcount(book):
-  wordcount = 0
-  #with open("books/frankenstein.txt") as f:
-  #  file_contents = f.read()
-  words = main(book).split()
-  for word in words:
-    wordcount += 1
-  return wordcount
-
-def charcount(book):
-  char_dict = {}
-  lowercase_book = main(book).lower()
-  char_list = list(lowercase_book)
-  for char in char_list:
-    if char in char_dict:
-      char_dict[char] += 1
-    else:
-      char_dict[char] = 1
-  return char_dict
-
-def sort_on(dict):
-  return dict['num']
-
-def report(book):
-  char_dict = charcount(book)
-  report_list = []
-  for key, value in char_dict.items():
-    if key.isalpha():
-      report_list.append({'char': key, 'num': value})
-  report_list.sort(reverse=True, key=sort_on)
-  for item in report_list:
-    char = item['char']
-    count = item['num']
-    print(f"The '{char}' character was found {count} times")
+from stats import (
+    get_num_words,
+    chars_dict_to_sorted_list,
+    get_chars_dict,
+)
 
 
-# main("books/frankenstein.txt")
-# wordcount()
-# char_count = charcount("books/frankenstein.txt")
-# print(report("books/frankenstein.txt"))
-print("-- Begin report of books/frankenstein.txt")
-word_count = wordcount("books/frankenstein.txt")
-print(f"{word_count} words found in the document")
-report("books/frankenstein.txt")
-print("--- End report ---")
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+    print_report(book_path, num_words, chars_sorted_list)
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+def print_report(book_path, num_words, chars_sorted_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
+
+    print("============= END ===============")
+
+
+main()
